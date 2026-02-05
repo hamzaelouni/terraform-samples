460e243ff04560580c9a4c56852e7fa94350b01f
hamzaelouni <helouni@partner.auchan.fr>
2026-02-05 17:08:01 +0100

    declare providers

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# terraform-samples
+To enable debug logging for terraform commands :
+- export environnement variable TF_LOG : `export TF_LOG=DEBUG` , we can use other levels like TRACE
+- execute terraform command
---
+++ providers/main.tf
@@ -0,0 +1,12 @@
+### providers example
+## azure resource manager
+provider "azurerm" {
+  #if i didn't specify the version, tf will download the latest version from tf registry
+  version = "3.0"
+  features {}
+}
+
+provider "aws" {
+  version = "4.0"
+  region = "us-east-1"
+}
